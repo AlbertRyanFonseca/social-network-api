3f2580617c681df58fd3dfe126251b2bad4b7b8c
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 23:27:05 -0700

    Added the api routes for the app to completely work.

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,25 +1,22 @@
 const router = require('express').Router();
 
 const {
-    getAllThought,
-    addThought,
+    getAllThoughts,
+    createThought,
     removeThought,
     addReaction,
     removeReaction
 } = require('../../controllers/thought-controller');
 
-router.route('/').get(getAllThought);
+router.route('/').get(getAllThoughts);
 
-// /api/thoughts/<userId>
-router.route('/:userId').post(addThought);
+router.route('/:userId').post(createThought);
 
-// /api/thoughts/<pizzaId>/<thoughtId>
 router
     .route('/:userId/:thoughtId')
     .put(addReaction)
     .delete(removeThought);
 
-// /api/thoughts/<pizzaId>/<thoughtId>/<replyId>
 router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
 
 module.exports = router;
